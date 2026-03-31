e0bce8bf78a5f91b3c07da60ded4185ba0bfc5ee
sukhrajsinghbhatia05 <sukhrajsinghbhatia05@gmail.com>
2026-03-31 21:19:32 +0530

    Create DivisionCalculator.java

---
+++ DivisionCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class DivisionCalculator 
+{
+    public static void main(String[] args) 
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        try 
+        {
+            System.out.print("Enter numerator: ");
+            int num = sc.nextInt();
+            System.out.print("Enter denominator: ");
+            int den = sc.nextInt();
+
+            int result = num / den;
+            System.out.println("Result: " + result);
+        }
+        catch (ArithmeticException e) 
+        {
+            // Specific catch for dividing by zero
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
