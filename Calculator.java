97413f0eafaa0e5ad991deeb248a38af2acd2e6a
sukhrajsinghbhatia05 <sukhrajsinghbhatia05@gmail.com>
2026-03-31 20:06:35 +0530

    Create Calculator.java

---
+++ Calculator.java
@@ -0,0 +1,5 @@
+class Calculator {
+    void add(int a, int b) { System.out.println(a + b); } [cite: 39]
+    void add(int a, int b, int c) { System.out.println(a + b + c); } [cite: 39]
+    void add(double a, double b) { System.out.println(a + b); } [cite: 39]
+}
