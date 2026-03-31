2ef91b5df92776b8b7914935ac315dc7d597e85d
sukhrajsinghbhatia05 <sukhrajsinghbhatia05@gmail.com>
2026-03-31 20:14:49 +0530

    Update Employee.java

--- Employee.java
+++ Employee.java
@@ -1,15 +1,27 @@
-class Employee {
-    int empno; String ename; [cite: 19]
-    float basic, hra, da, netpay; [cite: 19]
+class Employee 
+{
+    int empno;
+    String ename;
+    float basic, hra, da, netpay;
 
-    float Calculate() { return basic + hra + da; } [cite: 21]
+    // Calculate sum of components
+    float Calculate() 
+    {
+        return basic + hra + da;
+    }
 
-    void haveData(int id, String n, float b, float h, float d) { [cite: 22]
-        empno = id; ename = n; basic = b; hra = h; da = d;
+    void havedata(int id, String name, float b, float h, float d) 
+    {
+        empno = id;
+        ename = name;
+        basic = b;
+        hra = h;
+        da = d;
         netpay = Calculate();
     }
 
-    void dispData() { [cite: 23]
-        System.out.println(ename + " Net Pay: " + netpay);
+    void dispdata() 
+    {
+        System.out.println("ID: " + empno + " Name: " + ename + " Net Pay: " + netpay);
     }
 }
