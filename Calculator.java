e9c8797f4cd29602e41ed5ed651f9ac243bb78b9
sukhrajsinghbhatia05 <sukhrajsinghbhatia05@gmail.com>
2026-03-31 20:17:20 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,18 @@
-class Calculator {
-    void add(int a, int b) { System.out.println(a + b); } [cite: 39]
-    void add(int a, int b, int c) { System.out.println(a + b + c); } [cite: 39]
-    void add(double a, double b) { System.out.println(a + b); } [cite: 39]
+class Calculator 
+{
+    // Method overloading with different parameter lists
+    void add(int a, int b) 
+    {
+        System.out.println("Sum of 2 ints: " + (a + b));
+    }
+
+    void add(int a, int b, int c) 
+    {
+        System.out.println("Sum of 3 ints: " + (a + b + c));
+    }
+
+    void add(double a, double b) 
+    {
+        System.out.println("Sum of 2 doubles: " + (a + b));
+    }
 }
