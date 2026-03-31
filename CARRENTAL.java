f2f006e9fff75ae53928ae51750c26b95e8cb05c
sukhrajsinghbhatia05 <sukhrajsinghbhatia05@gmail.com>
2026-03-31 20:13:49 +0530

    Update CARRENTAL.java

--- CARRENTAL.java
+++ CARRENTAL.java
@@ -1,21 +1,38 @@
-class c {
-    int CarId; [cite: 8]
-    String CarType; [cite: 8]
-    float Rent; [cite: 8]
+class CARRENTAL 
+{
+    int CarId;
+    String CarType;
+    float Rent;
 
-    void GetCar(int id, String type) { [cite: 9]
-        CarId = id; CarType = type;
-        Rent = GetRent();
+    // Method to accept ID and Type
+    void GetCar(int id, String type) 
+    {
+        this.CarId = id;
+        this.CarType = type;
+        this.Rent = GetRent();
     }
 
-    float GetRent() { [cite: 10]
-        if (CarType.equals("Small Car")) return 1000;
-        if (CarType.equals("Van")) return 800;
-        if (CarType.equals("SUV")) return 2500;
+    // Method to return rent based on car type
+    float GetRent() 
+    {
+        if (CarType.equalsIgnoreCase("Small Car")) 
+        {
+            return 1000;
+        }
+        else if (CarType.equalsIgnoreCase("Van")) 
+        {
+            return 800;
+        }
+        else if (CarType.equalsIgnoreCase("SUV")) 
+        {
+            return 2500;
+        }
         return 0;
     }
 
-    void ShowCar() { [cite: 11]
+    // Method to view contents
+    void ShowCar() 
+    {
         System.out.println("ID: " + CarId + " Type: " + CarType + " Rent: " + Rent);
     }
 }
