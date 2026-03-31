a2b5526d46d87e4c5546bb3b54c99cd48941149e
sukhrajsinghbhatia05 <sukhrajsinghbhatia05@gmail.com>
2026-03-31 20:14:13 +0530

    Update Resort.java

--- Resort.java
+++ Resort.java
@@ -1,18 +1,30 @@
-class Resort {
-    int RNo, Days; [cite: 13]
-    String Name; [cite: 13]
-    float Charges; [cite: 13]
+class Resort 
+{
+    int RNo, Days;
+    String Name;
+    float Charges;
 
-    float Compute() { [cite: 15]
+    // Calculate and return amount
+    float Compute() 
+    {
         float amount = Days * Charges;
-        return (amount > 11000) ? 1.02f * amount : amount;
+        if (amount > 11000) 
+        {
+            return 1.02f * amount;
+        }
+        return amount;
     }
 
-    void GetInfo(int r, String n, float c, int d) { [cite: 16]
-        RNo = r; Name = n; Charges = c; Days = d;
+    void Getinfo(int r, String n, float c, int d) 
+    {
+        RNo = r;
+        Name = n;
+        Charges = c;
+        Days = d;
     }
 
-    void DispInfo() { [cite: 17]
-        System.out.println(RNo + " " + Name + " Total: " + Compute());
+    void DispInfo() 
+    {
+        System.out.println("Room: " + RNo + " Name: " + Name + " Total: " + Compute());
     }
 }
