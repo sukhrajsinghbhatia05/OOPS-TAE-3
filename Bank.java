479aa6d7c5b399e2cdd602c52a664f3a387a3e5b
sukhrajsinghbhatia05 <sukhrajsinghbhatia05@gmail.com>
2026-03-31 20:17:37 +0530

    Update Bank.java

--- Bank.java
+++ Bank.java
@@ -1,3 +1,19 @@
-interface Bank { float getInterestRate(); } [cite: 41]
-class SBI implements Bank { public float getInterestRate() { return 5.0f; } } [cite: 41]
-class HDFC implements Bank { public float getInterestRate() { return 6.0f; } } [cite: 41]
+interface Bank 
+{
+    float getInterestRate();
+}
+
+class SBI implements Bank 
+{
+    public float getInterestRate() { return 5.0f; }
+}
+
+class HDFC implements Bank 
+{
+    public float getInterestRate() { return 6.0f; }
+}
+
+class ICICI implements Bank 
+{
+    public float getInterestRate() { return 5.5f; }
+}
