eab7f93485028c13f72513e70ea2d0e1d37a6444
sukhrajsinghbhatia05 <sukhrajsinghbhatia05@gmail.com>
2026-03-31 20:15:44 +0530

    Update Rectangle.java

--- Rectangle.java
+++ Rectangle.java
@@ -1,11 +1,23 @@
-class Rectangle {
+class Rectangle 
+{
     int length, breadth;
-    Rectangle(int l, int b) { length = l; breadth = b; } [cite: 24]
-    int Area() { return length * breadth; } [cite: 24]
 
-    public static void main(String[] args) {
-        Rectangle r1 = new Rectangle(4, 5); [cite: 24]
-        Rectangle r2 = new Rectangle(5, 8); [cite: 24]
+    // Parameters passed to constructor
+    Rectangle(int l, int b) 
+    {
+        length = l;
+        breadth = b;
+    }
+
+    int Area() 
+    {
+        return length * breadth;
+    }
+
+    public static void main(String[] args) 
+    {
+        Rectangle r1 = new Rectangle(4, 5);
+        Rectangle r2 = new Rectangle(5, 8);
         System.out.println("Area 1: " + r1.Area());
         System.out.println("Area 2: " + r2.Area());
     }
